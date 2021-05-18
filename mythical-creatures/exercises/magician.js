4f5b518c197e2d29fa82e8da6b1159ac90cb53f4
Gabrielle Joyce <68332132+gaj23@users.noreply.github.com>
2021-05-18 11:21:16 -0500

    Pass all existing tests

--- mythical-creatures/exercises/magician.js
+++ mythical-creatures/exercises/magician.js
@@ -2,7 +2,32 @@ class Magician {
   constructor(magic) {
     this.name = `The Great ${magic.name}`
     this.assistant = magic.assistant
-    this.favoriteAccessory = magic.favoriteAccessory || 'top hat'
+    this.favoriteAccessory = magic.clothing || 'top hat'
+    this.confidencePercentage = 10
+    this.assistant = false || magic.assistant
+  }
+
+  performIncantation = magicWords => {
+    return `${magicWords.toUpperCase()}!`
+  }
+
+  performTrick = () => {
+    this.confidencePercentage += 10
+
+    switch (this.favoriteAccessory) {
+      case 'top hat':
+        return 'PULL RABBIT FROM TOP HAT'
+      default:
+        return 'PULL DOVE FROM SLEEVE'
+    }
+  }
+
+  performShowStopper = () => {
+    if (this.confidencePercentage >= 100 && this.assistant === true) {
+      return 'WOW! The magician totally just sawed that person in half!' }
+    else {
+      return 'Oh no, this trick is not ready!'
+    }
   }
 }
 
